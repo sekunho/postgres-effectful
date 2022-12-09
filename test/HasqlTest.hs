70f93d2ad14437072500cbee5b2e679fb8c1a1a2
sekun <sekun@duck.com>
2022-12-09 19:07:20 +0800

    test: Run tests in CI (#5)

--- test/HasqlTest.hs
+++ test/HasqlTest.hs
@@ -13,11 +13,13 @@ module HasqlTest where
 
 -------------------------------------------------------------------------------
 
-import Data.Either (fromRight)
 import Data.Int (Int32)
+import qualified System.Environment as Environment (lookupEnv)
 
 -------------------------------------------------------------------------------
 
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as Char8 (pack)
 import Effectful (runEff)
 import Effectful.Hasql (query, runDB)
 import Hasql.Connection (Connection, acquire)
@@ -32,6 +34,16 @@ import Hasql.TH (singletonStatement)
 import Test.Hspec (Spec, describe, it, shouldReturn)
 import Test.Tasty
 import Test.Tasty.Hspec (testSpec)
+
+-------------------------------------------------------------------------------
+
+data TestEnv = MkTestEnv
+  { _dbHost :: ByteString
+  , _dbUser :: ByteString
+  , _dbPassword :: ByteString
+  , _dbName :: ByteString
+  }
+  deriving (Show)
 
 -------------------------------------------------------------------------------
 
@@ -73,13 +85,41 @@ select1 connection =
 badSelect :: Connection -> IO (Either QueryError Int32)
 badSelect connection =
   runEff . runDB connection $
-    let session = Session.statement () [singletonStatement| SELECT col_404 :: INT from table_404 |]
+    let session =
+          Session.statement
+            ()
+            [singletonStatement| SELECT col_404 :: INT from table_404 |]
      in query session
 
 -------------------------------------------------------------------------------
 -- Helpers
 
 setup :: IO Connection
-setup = acquire settings >>= pure . fromRight (error "kaboom")
+setup = do
+  testEnv <- getTestEnv
+  connection <- acquire (mkSettings testEnv)
+
+  case connection of
+    Right connection' -> pure connection'
+    Left e -> error (show e)
  where
-  settings = Connection.settings "localhost" 5432 "postgres" "" "postgres"
+  mkSettings (MkTestEnv dbHost dbUser dbPassword dbName) =
+    Connection.settings dbHost 5432 dbUser dbPassword dbName
+
+  getTestEnv :: IO TestEnv
+  getTestEnv = do
+    pgHost <- Environment.lookupEnv "POSTGRES_HOST"
+    pgUser <- Environment.lookupEnv "POSTGRES_USER"
+    pgPassword <- Environment.lookupEnv "POSTGRES_PASSWORD"
+    pgDbName <- Environment.lookupEnv "POSTGRES_DB_NAME"
+
+    case env pgHost pgUser pgPassword pgDbName of
+      Just env' -> pure env'
+      Nothing -> error "Lacking test environment variable(s) for Postgres server"
+   where
+    env pgHost pgUser pgPassword pgDbName =
+      MkTestEnv
+        <$> (Char8.pack <$> pgHost)
+        <*> (Char8.pack <$> pgUser)
+        <*> (Char8.pack <$> pgPassword)
+        <*> (Char8.pack <$> pgDbName)
