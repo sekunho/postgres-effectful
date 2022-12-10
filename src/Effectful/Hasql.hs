8f2567814c84543940598a9f0cb3f1d778db149d
SEKUN <sekun@hey.com>
2022-12-10 14:27:12 +0800

    docs: Document packages (#6)

--- src/Effectful/Hasql.hs
+++ src/Effectful/Hasql.hs
@@ -7,7 +7,12 @@
 
 -------------------------------------------------------------------------------
 
-module Effectful.Hasql (DB', query, runDB) where
+-- | Contains the statically dispatched effect, and relevant functions for a
+-- single connection-based DB effect. If you need to take a connection from a
+-- pool of connections, you could look into @hasql-pool-effectful@ instead.
+--
+-- This adapts the @hasql@ package.
+module Effectful.Hasql (DB', query, runDB') where
 
 -------------------------------------------------------------------------------
 
@@ -27,25 +32,46 @@ import qualified Hasql.Session as Session
 -- | A single connection DB effect
 data DB' :: Effect
 
+{- | Used to determine the dispatch type, and whether or not it's permitted to
+ perform side effects.
+-}
 type instance DispatchOf DB' = 'Static 'WithSideEffects
+
+{- | Contains the initial representation of the @DB'@ effect, which is a single
+ DB connection.
+-}
 newtype instance StaticRep DB' = DB' Connection
 
 -------------------------------------------------------------------------------
 
-runDB ::
+-- | Run a @DB'@ effect with the initial representation/environment: a DB
+-- connection.
+runDB' ::
   forall (es :: [Effect]) (a :: Type).
   IOE :> es =>
+  -- | Will be put in the initial environment. Represents a single DB connection
   Connection ->
   Eff (DB' : es) a ->
   Eff es a
-runDB = Static.evalStaticRep . DB'
+runDB' = Static.evalStaticRep . DB'
 
 unDB :: StaticRep DB' -> Connection
 unDB (DB' connection) = connection
 
+{- | Executes a DB session
+
+ >>> import qualified Hasql.Session
+ >>> import Hasql.TH (singletonStatement)
+ >>> import Effectful (runEff)
+
+ >>> session = Session.statement () [singletonStatement | SELECT 1 :: INT |]
+ >>> runEff . runDB' connection $ query session
+ Right 1
+-}
 query ::
   forall (es :: [Effect]) (a :: Type).
   (DB' :> es) =>
+  -- | Database session containing one or more statements
   Session a ->
   Eff es (Either QueryError a)
 query session =
