2e88e7924d79872938b51ad6e332c5ee8e6540b1
SEKUN <software@sekun.net>
2024-12-19 01:25:57 +0800

    chore: Bump packages

--- flake.nix
+++ flake.nix
@@ -3,8 +3,14 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
+  nixConfig = {
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    # allow-import-from-derivation = "true";
+  };
+
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
       let
         name = "hasql-effectful";
 
@@ -14,7 +20,7 @@
             hasql-effectful =
               final.haskell-nix.project' {
                 src = ./.;
-                compiler-nix-name = "ghc925";
+                compiler-nix-name = "ghc9101";
 
                 shell = {
                   buildInputs = with pkgs; [
@@ -24,7 +30,7 @@
 
                   tools = {
                     cabal = {};
-                    hlint = {};
+                    # hlint = {};
                     haskell-language-server = {};
                     fourmolu = {};
                   };
